7fc8a60d71b98909e98fd5f5a1ec262dc3bab70f
Anilt96 <110937736+Anilt96@users.noreply.github.com>
2022-08-18 23:31:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,27 +14,27 @@ provider "azurerm" {
 
 resource "azurerm_resource_group" "main" {
     name     = "resources"
-    location = "${var.location}"
+    location = "South India"
 }
 
 resource "azurerm_virtual_network" "main" {
     name                 = "network"
     address_space        = ["10.0.0.0/16"]
-    location             = "${var.location}"
-    resource_group_name  = "${azurerm_resource_group.main.name}"
+    location             = "South India"
+    resource_group_name  = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "internal" {
     name                 = "internal"
-    resource_group_name  = "${azurerm_resource_group.main.name}"
-    virtual_network_name = "${azurerm_virtual_network.main.name}"
+    resource_group_name  = azurerm_resource_group.main.name
+    virtual_network_name = azurerm_virtual_network.main.name
     address_prefixes       = ["10.0.2.0/24"]
 }
 
 resource "azurerm_network_security_group" "main" {
     name             = "sg"
-    location         = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.main.name}"
+    location         = "South India"
+    resource_group_name = azurerm_resource_group.main.name
 
     security_rule {
         name                        = "HTTP"
@@ -63,34 +63,34 @@ resource "azurerm_network_security_group" "main" {
 
 resource "azurerm_network_interface" "main" {
     name                = "nic"
-    location            = "${var.location}"
-    resource_group_name = "${azurerm_resource_group.main.name}"
+    location            = "South India"
+    resource_group_name = azurerm_resource_group.main.name
     ip_configuration {
         name                       = "testconfiguration1"
         subnet_id                  =  azurerm_subnet.internal.id
         private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = "${azurerm_public_ip.main.id}"
+        public_ip_address_id          = azurerm_public_ip.main.id
     }
 }
 
 resource "azurerm_network_interface_security_group_association" "main" {
-    network_interface_id            = "${azurerm_network_interface.main.id}"
-    network_security_group_id       = "${azurerm_network_security_group.main.id}"
+    network_interface_id            = azurerm_network_interface.main.id
+    network_security_group_id       = azurerm_network_security_group.main.id
 }
 
 resource "azurerm_public_ip" "main" {
     name                    = "ip"
-    location                = "${var.location}"
-    resource_group_name     = "${azurerm_resource_group.main.name}"
+    location                = "South India"
+    resource_group_name     = azurerm_resource_group.main.name
     allocation_method       = "Dynamic"
     domain_name_label       = "jenkins"
 }
 
 resource "azurerm_virtual_machine" "main" {
     name                              = "vm"
-    location                          = "${azurerm_resource_group.main.location}"
-    resource_group_name               = "${azurerm_resource_group.main.name}"
-    network_interface_ids             = ["${azurerm_network_interface.main.id}"]
+    location                          = azurerm_resource_group.main.location
+    resource_group_name               = azurerm_resource_group.main.name
+    network_interface_ids             = azurerm_network_interface.main.id
     vm_size                           = "Standard_B2ms"
 
     storage_image_reference {
